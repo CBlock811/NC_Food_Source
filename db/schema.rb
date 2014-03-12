ae3cf986d5bf932a33efcab171823a347eee21ec
Christian <Christian.Block@yahoo.com>
2014-03-11 20:32:05 -0700

    added supplier, product models

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140312032912) do
+
+  create_table "products", force: true do |t|
+    t.string   "name"
+    t.integer  "price"
+    t.integer  "supplier_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "products", ["supplier_id"], name: "index_products_on_supplier_id"
+
+  create_table "suppliers", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
