18a0e6701364e9385be2d3df3e3980af3999b121
vagrant <vagrant@rails-dev-box>
2014-03-20 03:04:29 +0000

    adjusted farms route for production

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 NCFoodSource::Application.routes.draw do
   
+  devise_for :users
   devise_for :admins
   resources :suppliers
-
-  devise_for :users
   
+  match "farms", to: 'suppliers#index', via: :get
   match "about", to: 'welcome#about', via: :get
   match "blog", to: 'welcome#blog', via: :get
   match "contact", to: 'welcome#contact', via: :get
