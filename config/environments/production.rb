5b0f30ceffca67171e2051255d82651734803505
vagrant <vagrant@rails-dev-box>
2014-03-28 13:04:33 +0000

    production through bloc

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,14 @@ NCFoodSource::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -87,16 +87,6 @@ NCFoodSource::Application.configure do
   }
 }
   
-  config.action_mailer.default_url_options = { host: 'bloccit.herokuapp.com' }
-
-  ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
-}
+  config.action_mailer.default_url_options = { host: 'nc-food-source.herokuapp.com' }
 
 end
